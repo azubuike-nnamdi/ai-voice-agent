03e1960c172841bbedc28ee5fa1b4a55e227b557
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-06-16 20:54:03 +0100

    chore: include navbar :zap:

---
+++ components/common/navbar.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <Link href='/' className='navbar-brand'>
+        <div className='flex items-center gap-2.5 cursor-pointer'>
+          <Image src='/images/logo.svg' alt='Logo' width={40} height={40} />
+        </div>
+      </Link>
+      <div className='flex items-center gap-8'>
+        <Link href='/' className='navbar-item'>
+          Home
+        </Link>
+        <Link href='/about' className='navbar-item'>
+          About
+        </Link>
+        <Link href='/contact' className='navbar-item'>
+          Contact
+        </Link>
+      </div>
+
+    </nav>
+  )
+}
