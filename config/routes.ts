e89d91d312accc87fbf45e6852c5772c79193f3d
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-06-16 21:21:31 +0100

    feat: config route :zap:

---
+++ config/routes.ts
@@ -0,0 +1,11 @@
+const HOME_URL: string = '/';
+const COMPANIONS_URL: string = '/companions';
+const COMPANION_DETAIL_URL: string = '/companions/:id';
+const MY_JOURNEY_URL: string = '/my-journey';
+
+export {
+  HOME_URL,
+  COMPANIONS_URL,
+  COMPANION_DETAIL_URL,
+  MY_JOURNEY_URL
+}
