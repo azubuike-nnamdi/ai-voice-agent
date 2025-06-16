9b8044ad293234374948cac60a56fc996e241a57
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-06-16 21:22:48 +0100

    chore: update navbar style :paintbrush:

--- data/index.ts
+++ data/index.ts
@@ -2,7 +2,7 @@ import { COMPANIONS_URL, HOME_URL, MY_JOURNEY_URL } from "@/config/routes"
 
 const navItems = [
   { label: 'Home', href: HOME_URL },
-  { label: 'Companions', href: COMPANIONS_URL },
+  { label: 'Learning Companions', href: COMPANIONS_URL },
   { label: 'My Journey', href: MY_JOURNEY_URL },
 ]
 
