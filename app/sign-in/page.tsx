7b68a8a392f733494928e186bec5fc4cea66080e
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-06-24 20:34:20 +0100

    chore: add a configured sign in page for authentication :zap:

--- app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Sign In</h1>
-      <p>Please sign in to continue.</p>
-      {/* Add your sign-in form or components here */}
-    </div>
-  )
-}
